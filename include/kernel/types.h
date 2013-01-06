857d9a4ceab0018102c9e3fb2b9f8144f5e5fe82
salmabad <srodr063@fiu.edu>
2013-01-06 15:38:53 -0500

    adding type definitions

---
+++ include/kernel/types.h
@@ -0,0 +1,19 @@
+#ifndef TYPES_H_
+#define TYPES_H_
+
+typedef int int32_t;
+typedef char int8_t;
+typedef short int16_t;
+typedef unsigned int uint32_t;
+typedef unsigned char uint8_t;
+typedef unsigned short uint16_t;
+
+typedef int8_t s8;
+typedef int16_t s16;
+typedef int32_t s32;
+
+typedef uint8_t u8;
+typedef uint16_t u16;
+typedef uint32_t u32;
+
+#endif
