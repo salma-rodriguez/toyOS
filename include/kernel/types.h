73f577a013b6f00f55e967e4fd8458e94b52135a
salmabad <srodr063@fiu.edu>
2013-01-07 16:19:28 -0500

    adding new types

--- include/kernel/types.h
+++ include/kernel/types.h
@@ -1,19 +1,35 @@
+// simple type definitions
+// next: 1. make POSIX-compliant by using posix_types.h
+// 	 2. declare assembly types in another file (maybe)
+
 #ifndef TYPES_H_
 #define TYPES_H_
 
-typedef int int32_t;
-typedef char int8_t;
-typedef short int16_t;
-typedef unsigned int uint32_t;
-typedef unsigned char uint8_t;
-typedef unsigned short uint16_t;
+typedef char 		__s8;
+typedef short 		__s16;
+typedef int 		__s32;
+typedef long		__s64;
 
-typedef int8_t s8;
-typedef int16_t s16;
-typedef int32_t s32;
+typedef unsigned char 	__u8;
+typedef unsigned short 	__u16;
+typedef unsigned int 	__u32;
+typedef unsigned long	__u64;
 
-typedef uint8_t u8;
-typedef uint16_t u16;
-typedef uint32_t u32;
+typedef 	__s8 	int8_t;
+typedef 	__s16 	int16_t;
+typedef 	__s32 	int32_t;
+typedef 	__u8 	uint8_t;
+typedef 	__u16 	uint16_t;
+typedef 	__u32 	uint32_t;
+
+#ifndef _SIZE_T
+#define _SIZE_T
+typedef unsigned int size_t;
+#endif
+
+#ifndef _SSIZE_T
+#define _SSIZE_T
+typedef int ssize_t;
+#endif
 
 #endif
