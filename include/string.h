7ee2a9058da94998a1dbc27f7602129d3cd05a81
salmabad <srodr063@fiu.edu>
2013-01-07 16:20:06 -0500

    shuffling things around

--- include/string.h
+++ include/string.h
@@ -10,13 +10,20 @@
 typedef unsigned int size_t;
 #endif
 
-#ifndef __asm__
 #define __asm__ asm volatile
+
+#ifndef __always_inline
+#define __always_inline inline
 #endif
 
-extern inline int strlen(const char * s);
+extern inline size_t strlen(const char *s);
 extern inline int strcmp(const char *cs, const char *ct);
-extern inline char * strcat(char * dest, const char * src);
-extern inline char * strcpy(char * dest, const char * src);
+extern inline char *strcat(char *dest, const char *src);
+extern inline char *strcpy(char *dest, const char *src);
+extern inline void *memchr(const void *cs, int c, size_t count);
+extern inline int memcmp(const void *cs, const void *ct, size_t n);
+extern void *memmove(void *dest, const void *src, size_t n);
+extern __always_inline void *memcpy(void *dest, const void *src, size_t n);
+extern __always_inline void *memset(void *s, char c, size_t n);
 
 #endif
