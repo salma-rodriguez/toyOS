4948f844a099c0957c1910c5443346f7846ca078
Salma Rodriguez <srodr063@fiu.edu>
2013-01-11 18:16:33 -0500

    fixing some minor misspell

--- include/asm/isr.h
+++ include/asm/isr.h
@@ -7,5 +7,5 @@ typedef struct registers
 	uint32_t edi, esi, ebp, esp, ebx, edx, ecx, eax;
 	uint32_t int_no, err_code;
 
-	uint32_t eip, cs, eflags, userresp, ss;
+	uint32_t eip, cs, eflags, useresp, ss;
 } registers_t;
