ca3733b55564d2615f748770026468bed5a82b21
Salma Rodriguez <srodr063@fiu.edu>
2013-01-13 01:13:05 -0500

    cleaning some code

--- kernel/tables.c
+++ kernel/tables.c
@@ -11,7 +11,6 @@ static void gdt_init();
 static void idt_init();
 extern void gdt_flush(uint32_t);
 extern void idt_flush(uint32_t);
-static void gdt_set_gate(int32_t, uint32_t, uint32_t, uint8_t, uint8_t);
 static void idt_set_gate(uint8_t, uint32_t, uint16_t, uint8_t);
 
 gdt_entry_t gdt_entries[5];
@@ -25,34 +24,16 @@ void init_descriptor_tables()
 	idt_init();
 }
 
-/* static void gdt_flush(uint32_t gdt_ptr)
-{
-	int __res;
-	__asm__ __volatile__("lgdt %0\t\n"
-			"movl $0x10, %%a\t\n"
-			"movl %%a, %%ds\t\n"
-			"movl %%a, %%e\t\n"
-			"movl %%a, %%f\t\n"
-			"movl %%a, %%g\t\n"
-			"movl %%a, %%s\t\n"
-			"jmp $0x08 flushf\n"
-			"flush:\tret"
-			: "=a" (__res)
-			: "0" (gdt_ptr)
-			: "memory");
-	return __res;
-} */
-
 static void gdt_init()
 {
 	gdt_ptr.limit = (sizeof(gdt_entry_t) * 5) - 1;
 	gdt_ptr.base = (uint32_t)&gdt_entries;
 	
-	gdt_set_gate(0, 0, 0, 0, 0);
-	gdt_set_gate(1, 0, MAXADDR, 0x9A, 0xCF);
-	gdt_set_gate(2, 0, MAXADDR, 0x92, 0xCF);
-	gdt_set_gate(3, 0, MAXADDR, 0xFA, 0xCF);
-	gdt_set_gate(4, 0, MAXADDR, 0xF2, 0xCF);
+	gdt_entries[0] = (gdt_entry_t) {0, 0, 0, 0, 0, 0};
+	gdt_entries[1] = GDT_CODE_GATE(0, 0, MAXADDR);
+	gdt_entries[2] = GDT_DATA_GATE(0, 0, MAXADDR);
+	gdt_entries[3] = GDT_CODE_GATE(3, 0, MAXADDR);
+	gdt_entries[4] = GDT_DATA_GATE(3, 0, MAXADDR);
 
 	gdt_flush((uint32_t)&gdt_ptr);
 }
@@ -98,19 +79,6 @@ static void idt_init()
 	idt_set_gate(31, (uint32_t)isr31, 0x08, 0x8E);
 
 	idt_flush((uint32_t)&idt_ptr);
-}
-
-static void gdt_set_gate(int32_t num, uint32_t base, uint32_t limit, uint8_t access, uint8_t gran)
-{
-	gdt_entries[num].base_l = (base & 0xFFFF);
-	gdt_entries[num].base_m = (base >> 16) & 0xFF;
-	gdt_entries[num].base_h = (base >> 24) & 0xFF;
-	
-	gdt_entries[num].limit_l = limit & 0xFFFF;
-	gdt_entries[num].granularity = (limit >> 16) & 0x0F;
-
-	gdt_entries[num].granularity |= gran & 0xF0;
-	gdt_entries[num].access = access;
 }
 
 static void idt_set_gate(uint8_t num, uint32_t base, uint16_t sel, uint8_t flags)
