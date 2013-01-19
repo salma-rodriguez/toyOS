49edf4f131af5087542f54fcd785f1cfa6c3d8be
salmabad <srodr063@fiu.edu>
2013-01-18 23:31:08 -0500

    moving gdt_flush into here and adding IRQ macros

--- kernel/interrupt.s
+++ kernel/interrupt.s
@@ -5,6 +5,22 @@ idt_flush:
 	lidt [eax]
 	ret
 
+global gdt_flush
+
+gdt_flush:
+	mov eax, [esp+4]
+	lgdt [eax]
+
+	mov ax, 0x10
+	mov ds, ax
+	mov es, ax
+	mov fs, ax
+	mov gs, ax
+	mov ss, ax
+	jmp 0x08:.flush
+.flush:
+	ret
+
 %macro ISR_NOERRCODE 1
 	global isr%1
 	isr%1:
@@ -20,6 +36,15 @@ idt_flush:
 		cli
 		push byte %1
 		jmp  isr_common_stub
+%endmacro
+
+%macro IRQ 2
+	global irq%1
+	irq%1:
+		cli
+		push byte 0
+		push byte %2
+		jmp  irq_common_stub
 %endmacro
 
 ISR_NOERRCODE 0
@@ -54,7 +79,24 @@ ISR_NOERRCODE 28
 ISR_NOERRCODE 29
 ISR_NOERRCODE 30
 ISR_NOERRCODE 31
+IRQ	0,    32
+IRQ	1,    33
+IRQ	2,    34
+IRQ	3,    35
+IRQ	4,    36
+IRQ	5,    37
+IRQ	6,    38
+IRQ	7,    39
+IRQ	8,    40
+IRQ	9,    41
+IRQ    10,    42
+IRQ    11,    43
+IRQ    12,    44
+IRQ    13,    45
+IRQ    14,    46
+IRQ    15,    47
 
+extern irq_handler
 extern isr_handler
 
 isr_common_stub:
@@ -71,11 +113,36 @@ isr_common_stub:
 
 	call isr_handler
 
-	pop eax
+	pop ebx
 	mov ds, ax
 	mov es, ax
 	mov fs, ax
 	mov gs, ax
+
+	popa
+	add esp, 8
+	sti
+	iret
+
+irq_common_stub:
+	pusha
+
+	mov ax, ds
+	push eax
+
+	mov ax, 0x10
+	mov ds, ax
+	mov es, ax
+	mov fs, ax
+	mov gs, ax
+
+	call irq_handler
+
+	pop ebx
+	mov ds, bx
+	mov es, bx
+	mov fs, bx
+	mov gs, bx
 
 	popa
 	add esp, 8
