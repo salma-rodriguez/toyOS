a1bebc5a57b4c1bb5a3df4a8460989538baba482
salmabad <srodr063@fiu.edu>
2013-01-18 23:34:05 -0500

    adding timer

---
+++ kernel/timer.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <kernel/types.h>
+#include <kernel/common.h>
+#include <kernel/isr.h>
+#include <kernel/timer.h>
+
+uint32_t tick = 0;
+
+void timer_callback(registers_t regs)
+{
+	tick++;
+	printf("Tick: %d\n", tick);
+}
+
+void init_timer(uint32_t frequency)
+{
+	register_interrupt_handler(IRQ0, &timer_callback);
+
+	// Value sent to the PIT is to divide input clock
+	// (1193180 Hz) by to get frequency. Important note:
+	// divisor must be small enough to fit into 16 bits.
+	uint32_t divisor = 1193180 / frequency;
+
+	// Send the  command byte.
+	outb(0x43, 0x36);
+
+	// Divisor has to be sent byte-wise; split into upper/lower
+	uint8_t l = (uint8_t)(divisor & 0xFF);
+	uint8_t h = (uint8_t)((divisor>>8) & 0xFF);
+
+	// Send the frequency divisor.
+	outb(0x40, l);
+	outb(0x40, h);
+}
