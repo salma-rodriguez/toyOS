09b0e709cc405ebf72b613c8d6959c561a4b4292
salmabad <srodr063@fiu.edu>
2013-01-19 20:28:47 -0500

    fixed clock not ticking

--- init/kernel.c
+++ init/kernel.c
@@ -8,61 +8,25 @@
 extern void init_descriptor_tables();
 
 int kmain(multiboot_info_t *mbd, unsigned int magic) {
-	char *cs, *ct;
-	monitor_clear();
 
 	if (magic != MULTIBOOT_BOOTLOADER_MAGIC) {
 		printf("boot magic is incorrect");
 		/* next: do something, like PANIC */
 	}
 
-	cs = "Hello, world!";
+	monitor_clear();
 
-	/* testing strcmp */
-
-	if (!strcmp(cs, "Hello, world!"))
-		printf("String is \'Hello, world!\'\n");
-	else
-		printf("String is not \'Hello, world!\'\n");
-
-	cs = "Hello, ";
-	ct = "world!\n"; 
-	
-	/* testing strcat */
-
-	printf(strcat(cs,ct));
-
-	/* testing strcpy */
-
-	strcpy(cs, "Hello, world!\n");
-	printf(cs);
-
-	/* testing strlen & printf */
-
-	cs = "Hello, world!\n";
-	printf("The length of the string is: %d\n", strlen(cs));
-
-	/* testing memset */
-
-	cs = 0x0;
-	printf("cs before memset: %s\n", cs);
-
-	cs = memset(cs, 'H', 14);
-	printf("cs after memset: %s\n", cs);
-	
 	/* initialize discriptor tables */
 
 	init_descriptor_tables();
-
-	/* testing interrupts */
-
-	__asm__ __volatile__("int $0x3");
-	__asm__ __volatile__("int $0x4");
-
-	/* testing IRQs */
-
-	__asm__ __volatile__("sti");
+			
 	init_timer(50);
 
+	// __asm__ __volatile__("int $0x3");
+	// __asm__ __volatile__("int $0x4");
+	
+	__asm__ __volatile__("sti");
+	__asm__ __volatile__("int $0x8");
+			
 	return 0;
 }
--- kernel/timer.c
+++ kernel/timer.c
@@ -6,7 +6,7 @@
 
 uint32_t tick = 0;
 
-void timer_callback(registers_t regs)
+void timer_callback(struct registers regs)
 {
 	tick++;
 	printf("Tick: %d\n", tick);
@@ -14,19 +14,22 @@ void timer_callback(registers_t regs)
 
 void init_timer(uint32_t frequency)
 {
-	register_interrupt_handler(IRQ0, &timer_callback);
+	uint8_t l, h;
+	uint32_t divisor;
+
+	register_interrupt_handler(IRQ0, timer_callback);
 
 	// Value sent to the PIT is to divide input clock
 	// (1193180 Hz) by to get frequency. Important note:
 	// divisor must be small enough to fit into 16 bits.
-	uint32_t divisor = 1193180 / frequency;
+	divisor = 1193180 / frequency;
 
-	// Send the  command byte.
+	// Send the command byte.
 	outb(0x43, 0x36);
 
 	// Divisor has to be sent byte-wise; split into upper/lower
-	uint8_t l = (uint8_t)(divisor & 0xFF);
-	uint8_t h = (uint8_t)((divisor>>8) & 0xFF);
+	l = (uint8_t)(divisor & 0xFF);
+	h = (uint8_t)((divisor>>8) & 0xFF);
 
 	// Send the frequency divisor.
 	outb(0x40, l);
