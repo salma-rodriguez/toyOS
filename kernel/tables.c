10f7b502cfa772de45a963a13a55598787ab3b4d
salmabad <srodr063@fiu.edu>
2013-01-19 20:56:50 -0500

    new definitions and changes to the code

--- kernel/tables.c
+++ kernel/tables.c
@@ -5,14 +5,24 @@
 #include <kernel/isr.h>
 #include <kernel/common.h>
 
+#define CODE	0
+#define DATA	1
+
 #define NOINTS	256
 #define MAXADDR 0xFFFFFFFF
 
+#define IDT(isrnum) idt_set_gate(isrnum, (uint32_t)isr##isrnum)
+#define IRQ(num, irqnum) idt_set_gate(num, (uint32_t)irq##irqnum)
+
 static void gdt_init();
 static void idt_init();
+
 extern void gdt_flush(uint32_t);
 extern void idt_flush(uint32_t);
+
 static inline void idt_set_gate(uint8_t, uint32_t);
+static inline void gdt_set_gate(uint8_t, uint8_t, uint8_t, 
+		uint32_t, uint32_t);
 
 extern isr_t interrupt_handlers[];
 
@@ -25,6 +35,7 @@ void init_descriptor_tables()
 {
 	gdt_init();
 	idt_init();
+
 	memset(&interrupt_handlers, 0, sizeof(isr_t)*NOINTS);
 }
 
@@ -33,12 +44,12 @@ static void gdt_init()
 	gdt_ptr.limit = (sizeof(gdt_entry_t) * 5) - 1;
 	gdt_ptr.base = (uint32_t)&gdt_entries;
 	
-	gdt_entries[0] = (gdt_entry_t) {0, 0, 0, 0, 0, 0};
-	gdt_entries[1] = GDT_CODE_GATE(0, 0, MAXADDR);
-	gdt_entries[2] = GDT_DATA_GATE(0, 0, MAXADDR);
-	gdt_entries[3] = GDT_CODE_GATE(3, 0, MAXADDR);
-	gdt_entries[4] = GDT_DATA_GATE(3, 0, MAXADDR);
-
+	gdt_set_gate(0, 0, 0, 0, 0);
+	gdt_set_gate(1, 0, CODE, 0, MAXADDR);
+	gdt_set_gate(2, 0, DATA, 0, MAXADDR);
+	gdt_set_gate(3, 3, CODE, 0, MAXADDR);
+	gdt_set_gate(4, 3, DATA, 0, MAXADDR);
+	
 	gdt_flush((uint32_t)&gdt_ptr);
 }
 
@@ -58,64 +69,76 @@ static void remap_irq_table()
 
 static void idt_init()
 {
+	int i, j;
 	idt_ptr.limit = sizeof(idt_entry_t) * NOINTS - 1;
 	idt_ptr.base  = (uint32_t)&idt_entries;
 
 	memset(&idt_entries, 0, sizeof(idt_entry_t) * NOINTS);
 
-	// Remap irq table.
+	// Remap irq table
+	
 	remap_irq_table();
 		
-	idt_set_gate(0,  (uint32_t)isr0);
-	idt_set_gate(1,  (uint32_t)isr1);
-	idt_set_gate(2,  (uint32_t)isr2);
-	idt_set_gate(3,  (uint32_t)isr3);
-	idt_set_gate(4,  (uint32_t)isr4);
-	idt_set_gate(5,  (uint32_t)isr5);
-	idt_set_gate(6,  (uint32_t)isr6);
-	idt_set_gate(7,  (uint32_t)isr7);
-	idt_set_gate(8,  (uint32_t)isr8);
-	idt_set_gate(9,  (uint32_t)isr9);
-	idt_set_gate(10, (uint32_t)isr10);
-	idt_set_gate(11, (uint32_t)isr11);
-	idt_set_gate(12, (uint32_t)isr12);
-	idt_set_gate(13, (uint32_t)isr13);
-	idt_set_gate(14, (uint32_t)isr14);
-	idt_set_gate(15, (uint32_t)isr15);
-	idt_set_gate(16, (uint32_t)isr16);
-	idt_set_gate(17, (uint32_t)isr17);
-	idt_set_gate(18, (uint32_t)isr18);
-	idt_set_gate(19, (uint32_t)isr19);
-	idt_set_gate(20, (uint32_t)isr20);
-	idt_set_gate(21, (uint32_t)isr21);
-	idt_set_gate(22, (uint32_t)isr22);
-	idt_set_gate(23, (uint32_t)isr23);
-	idt_set_gate(24, (uint32_t)isr24);
-	idt_set_gate(25, (uint32_t)isr25);
-	idt_set_gate(26, (uint32_t)isr26);
-	idt_set_gate(27, (uint32_t)isr27);
-	idt_set_gate(28, (uint32_t)isr28);
-	idt_set_gate(29, (uint32_t)isr29);
-	idt_set_gate(30, (uint32_t)isr30);
-	idt_set_gate(31, (uint32_t)isr31);
-	idt_set_gate(32, (uint32_t)irq0);
-	idt_set_gate(33, (uint32_t)irq1);
-	idt_set_gate(34, (uint32_t)irq2);
-	idt_set_gate(35, (uint32_t)irq3);
-	idt_set_gate(36, (uint32_t)irq4);
-	idt_set_gate(37, (uint32_t)irq5);
-	idt_set_gate(38, (uint32_t)irq6);
-	idt_set_gate(39, (uint32_t)irq7);
-	idt_set_gate(40, (uint32_t)irq8);
-	idt_set_gate(41, (uint32_t)irq9);
-	idt_set_gate(42, (uint32_t)irq10);
-	idt_set_gate(43, (uint32_t)irq11);
-	idt_set_gate(44, (uint32_t)irq12);
-	idt_set_gate(45, (uint32_t)irq13);
-	idt_set_gate(46, (uint32_t)irq14);
-	idt_set_gate(47, (uint32_t)irq15);
+	// for (i = 0; i < 32; ++i)
+	
+	IDT(1);
+	IDT(2);
+	IDT(3);
+	IDT(4);
+	IDT(5);
+	IDT(6);
+	IDT(7);
+	IDT(8);
+	IDT(9);
+	IDT(10);
+	IDT(11);
+	IDT(12);
+	IDT(13);
+	IDT(14);
+	IDT(15);
+	IDT(16);
+	IDT(17);
+	IDT(18);
+	IDT(19);
+	IDT(20);
+	IDT(21);
+	IDT(22);
+	IDT(23);
+	IDT(24);
+	IDT(25);
+	IDT(26);
+	IDT(27);
+	IDT(28);
+	IDT(29);
+	IDT(30);
+	IDT(31);
+
+	// for (j = 0; j < 16; ++i, ++j) IRQ(i, j);
+	
+	IRQ(32, 0);
+	IRQ(33, 1);
+	IRQ(34, 2);
+	IRQ(35, 3);
+	IRQ(36, 4);
+	IRQ(37, 5);
+	IRQ(38, 6);
+	IRQ(39, 7);
+	IRQ(40, 8);
+	IRQ(41, 9);
+	IRQ(42, 10);
+	IRQ(43, 11);
+	IRQ(44, 12);
+	IRQ(45, 13);
+	IRQ(46, 14);
+	IRQ(47, 15);
 
 	idt_flush((uint32_t)&idt_ptr);
+}
+
+static inline void gdt_set_gate(uint8_t num, uint8_t privilege, 
+		uint8_t type, uint32_t base, uint32_t limit)
+{
+	gdt_entries[num] = GDT_SET_GATE(privilege, type, base, limit);
 }
 
 static inline void idt_set_gate(uint8_t num, uint32_t base)
