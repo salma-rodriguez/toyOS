a94390173e105547e2659fa9644300e42cafa27a
salmabad <srodr063@fiu.edu>
2013-01-21 21:21:35 -0500

    adding some new definitions

--- include/asm/common.h
+++ include/asm/common.h
@@ -3,9 +3,19 @@
 
 #include <kernel/types.h>
 
+#define EOI 0x20
+
+#define PICS 0xA0
+#define PICM 0x20
+
 __u8 inportb(__u16 port);
 __u16 inportw(__u16 port);
 void outportb(__u16 port, __u8 value);
+
+static inline void do_eoi()
+{
+	outportb(PICM, EOI);
+}
 
 static inline void enable_interrupts()
 {
@@ -16,6 +26,9 @@ static inline void disable_interrupts()
 {
 	__asm__ __volatile__("cli");
 }
+
+// this is a double fault, really
+// why does it work? I have no idea.
 
 static inline void start_system_timer()
 {
