42778525cc195f65ad3c7927c20efdeeae6cfd71
salmabad <srodr063@fiu.edu>
2013-01-27 12:26:58 -0500

    adding interrupt handlers

--- kernel/handler.c
+++ kernel/handler.c
@@ -5,14 +5,29 @@
 #include <kernel/printk.h>
 #include <kernel/panic.h>
 
-extern isr_t interrupt_handlers[INTS];
+// extern isr_t interrupt_handlers[INTS];
 
-extern void handle_pit_irq(struct registers *regs);
-extern void handle_page_fault(struct registers *regs);
+/** handlers for common interrupts **/
+
+/* these are scattered in different files
+ * but still initialized here; most don't do
+ * anything other than print to the screen,
+ * but still useful for debugging.
+ */
 
 void handle_divz_fault(struct registers *regs)
 {
 	printk("devision by zero!\n");
+}
+
+void handle_debugger(struct registers *regs)
+{
+	printk("debugger\n");
+}
+
+void handle_non_maskable(struct registers *regs)
+{
+	printk("non-maskable interrupt was triggered\n");
 }
 
 void handle_breakpoint(struct registers *regs)
@@ -35,14 +50,9 @@ void handle_inv_opcode(struct registers *regs)
 	printk("invalid opcode!\n");
 }
 
-void handle_gpf(struct registers *regs)
+void handle_co_pnavail(struct registers *regs)
 {
-	PANIC("global protection fault!\n");
-}
-
-void handle_seg_fault(struct registers *regs)
-{
-	printk("segmentation fault!\n");
+	printk("co-processor is not available!\n");
 }
 
 void handle_double_fault(struct registers *regs)
@@ -50,12 +60,141 @@ void handle_double_fault(struct registers *regs)
 	PANIC("double fault!");
 }
 
+void handle_seg_overrun(struct registers *regs)
+{
+	printk("segment overrun!\n");
+}
+
+void handle_seg_inv(struct registers *regs)
+{
+	printk("invalid segment!\n");
+}
+
+void handle_seg_np_fault(struct registers *regs)
+{
+	printk("segment not present!\n");
+}
+
+void handle_stack_fault(struct registers *regs)
+{
+	printk("stack fault was triggered\n");
+}
+
+void handle_gpf(struct registers *regs)
+{
+	PANIC("global protection fault!");
+}
+
+extern void handle_page_fault(struct registers *regs);
+
+void handle_seg_fault(struct registers *regs)
+{
+	printk("segmentation fault!\n");
+}
+
+void handle_math_fault(struct registers *regs)
+{
+	printk("math fault!\n");
+}
+
+extern void handle_pit_irq(struct registers *regs);
+
+void handle_keyboard_irq(struct registers *regs)
+{
+	printk("keyboard handler not implemented\n");
+}
+
+void handle_cascade_irq(struct registers *regs)
+{
+	printk("cascade handler not implemented\n", regs->int_no);
+}
+
+void handle_com2_irq(struct registers *regs)
+{
+	printk("serial port COM2 handler not implemented\n");
+}
+
+void handle_com1_irq(struct registers *regs)
+{
+	printk("serial port COM1 handler not implemented\n");
+}
+
+void handle_lpt2_irq(struct registers *regs)
+{
+	printk("lpt2 interrupt handler not implemented yet\n");
+}
+
+void handle_floppy_irq(struct registers *regs)
+{
+	printk("floppy interrupt handler not implemented\n");
+}
+
+void handle_lpt1_irq(struct registers *regs)
+{
+	printk("lpt1 interrupt handler not implemented\n");
+}
+
+void handle_cmos_irq(struct registers *regs)
+{
+	printk("CMOS real-time clock handler not implemented\n");
+}
+
+void handle_free1_irq(struct registers *regs)
+{
+	printk("first open interrupt handler not implemented\n");
+}
+
+void handle_free2_irq(struct registers *regs)
+{
+	printk("second open interrupt handler not implemented\n");
+}
+
+void handle_free3_irq(struct registers *regs)
+{
+	printk("third open interrupt handler not implemented\n");
+}
+
+void handle_mouse_irq(struct registers *regs)
+{
+	printk("mouse handler not implemented\n");
+}
+
+void handle_fpu_irq(struct registers *regs)
+{
+	printk("floating point unit handler not implemented\n");
+}
+
+void handle_phdd_irq(struct registers *regs)
+{
+	printk("first IDE channel handler not implemented\n");
+}
+
+void handle_shdd_irq(struct registers *regs)
+{
+	printk("second IDE channel handler not implemented\n");
+}
+
 void init_irq_handlers()
 {
 	printk("IRQ handlers...\t\t");
 
-	register_interrupt_handler(PIT, (isr_t)handle_pit_irq);
-	
+	register_interrupt_handler(PIT, 	(isr_t)handle_pit_irq);
+	register_interrupt_handler(KEYBOARD, 	(isr_t)handle_keyboard_irq);
+	register_interrupt_handler(CASCADE,	(isr_t)handle_cascade_irq);
+	register_interrupt_handler(COM2,	(isr_t)handle_com2_irq);
+	register_interrupt_handler(COM1,	(isr_t)handle_com1_irq);
+	register_interrupt_handler(LPT2,	(isr_t)handle_lpt2_irq);
+	register_interrupt_handler(FLOPPY,	(isr_t)handle_floppy_irq);
+	register_interrupt_handler(LPT1,	(isr_t)handle_lpt1_irq);
+	register_interrupt_handler(CMOS,	(isr_t)handle_cmos_irq);
+	register_interrupt_handler(FREE1,	(isr_t)handle_free1_irq);
+	register_interrupt_handler(FREE2, 	(isr_t)handle_free2_irq);
+	register_interrupt_handler(FREE3,	(isr_t)handle_free3_irq);
+	register_interrupt_handler(PS2_MOUSE,	(isr_t)handle_mouse_irq);
+	register_interrupt_handler(FPU,		(isr_t)handle_fpu_irq);
+	register_interrupt_handler(PATA_IDE,	(isr_t)handle_phdd_irq);
+	register_interrupt_handler(SATA_IDE,	(isr_t)handle_shdd_irq);
+
 	printk("done!\n");
 }
 
@@ -63,14 +202,23 @@ void init_fault_handlers()
 {
 	printk("fault handlers...\t");
 
+	register_interrupt_handler(SIG_DZEROV, (isr_t)handle_divz_fault);
+	register_interrupt_handler(SIG_DEBUGV, (isr_t)handle_debugger);
+	register_interrupt_handler(SIG_NMINTV, (isr_t)handle_non_maskable);
 	register_interrupt_handler(SIG_BPOINT, (isr_t)handle_breakpoint);
+	register_interrupt_handler(SIG_OVRFLO, (isr_t)handle_overflow);
 	register_interrupt_handler(SIG_BOUNDS, (isr_t)handle_bounds_err);
 	register_interrupt_handler(SIG_OPCODE, (isr_t)handle_inv_opcode);
-	register_interrupt_handler(SIG_GENPFV, (isr_t)handle_gpf);
-	register_interrupt_handler(SIG_DZEROV, (isr_t)handle_divz_fault);
-	register_interrupt_handler(SIG_SEGFLT, (isr_t)handle_seg_fault);
-	register_interrupt_handler(SIG_PAGEFV, (isr_t)handle_page_fault);
+	register_interrupt_handler(SIG_NAVAIL, (isr_t)handle_co_pnavail);
 	register_interrupt_handler(SIG_DOUBLE, (isr_t)handle_double_fault);
+	register_interrupt_handler(SIG_SEGOVR, (isr_t)handle_seg_overrun);
+	register_interrupt_handler(SIG_SEGINV, (isr_t)handle_seg_inv);
+	register_interrupt_handler(SIG_SEGNPR, (isr_t)handle_seg_np_fault);
+	register_interrupt_handler(SIG_STACKV, (isr_t)handle_stack_fault);
+	register_interrupt_handler(SIG_GENPFV, (isr_t)handle_gpf);
+	register_interrupt_handler(SIG_PAGEFV, (isr_t)handle_page_fault);
+	register_interrupt_handler(SIG_SEGFLT, (isr_t)handle_seg_fault);
+	register_interrupt_handler(SIG_MATHFV, (isr_t)handle_math_fault);
 
 	printk("done!\n");
 }
