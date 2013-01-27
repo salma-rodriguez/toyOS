3dd9d56e853de8a5393a6999072e887cb0c17c5e
salmabad <srodr063@fiu.edu>
2013-01-27 12:21:33 -0500

    adding generic kmalloc_int

--- mm/slab.c
+++ mm/slab.c
@@ -6,11 +6,13 @@
 extern uint32_t end;
 uint32_t placement_addr = (uint32_t)&end;
 
+static uint32_t kmalloc_int(size_t, int, uint32_t *);
+
 #define ALIGN() \
-	if ((placement_addr & 0xFFFFF000)) { \
+	if (placement_addr & 0xFFFFF000) { \
 		placement_addr &= 0xFFFFF000; \
 		placement_addr += 0x1000; \
-}
+	}
 
 #define PLACE(siz) ({ \
 	uint32_t tmp; \
@@ -19,36 +21,31 @@ uint32_t placement_addr = (uint32_t)&end;
 	tmp; \
 })
 
-/** generic kmalloc */
 uint32_t kmalloc(size_t siz)
 {
-	uint32_t temp;
-	temp = PLACE(siz);
-	return temp;
+	return kmalloc_int(siz, 0, NULL);
 }
 
-/** page aligned */
 uint32_t kmalloc_a(size_t siz)
 {
-	uint32_t temp;
-	ALIGN();
-	temp = PLACE(siz);
-	return temp;
+	return kmalloc_int(siz, 1, NULL);
 }
 
 uint32_t kmalloc_p(size_t siz, uint32_t *phys)
 {
-	uint32_t temp;
-	temp = PLACE(siz);
-	*phys = placement_addr;
-	return temp;
+	return kmalloc_int(siz, 0, phys);
 }
 
-uint32_t kmalloc_ap(size_t siz, int align, uint32_t *phys)
+uint32_t kmalloc_ap(size_t siz, uint32_t *phys)
+{
+	return kmalloc_int(siz, 1, phys);
+}
+
+static uint32_t kmalloc_int(size_t siz, int align, uint32_t *phys)
 {
 	uint32_t temp;
-	ALIGN();
+	if (align) ALIGN();
+	if (phys) *phys = placement_addr;
 	temp = PLACE(siz);
-	*phys = placement_addr;
 	return temp;
 }
