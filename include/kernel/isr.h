a3bd61bd0267b62110a0c884bebbe759067b67cc
salmabad <srodr063@fiu.edu>
2013-01-27 12:29:57 -0500

    changing definitions

--- include/kernel/isr.h
+++ include/kernel/isr.h
@@ -1,24 +1,43 @@
 #ifndef _ISR_H_
 #define _ISR_H_
 
+#include <asm/int_defs.h>
 #include <kernel/types.h>
 
-#define IRQ0 	32
-#define IRQ1 	33
-#define IRQ2	34
-#define IRQ3	35
-#define IRQ4	36
-#define IRQ5	37
-#define IRQ6	38
-#define IRQ7	39
-#define IRQ8	40
-#define IRQ9	41
-#define IRQ10	42
-#define IRQ11	43
-#define IRQ12	44
-#define IRQ13	45
-#define IRQ14	46
-#define IRQ15	47
+#define SIG_DZEROV	INT0
+#define SIG_DEBUGV	INT1
+#define SIG_NMINTV	INT2
+#define SIG_BPOINT	INT3
+#define SIG_OVRFLO	INT4
+#define SIG_BOUNDS	INT5
+#define SIG_OPCODE	INT6
+#define SIG_NAVAIL	INT7
+#define SIG_DOUBLE	INT8
+#define SIG_SEGOVR	INT9
+#define SIG_SEGINV	INT10
+#define SIG_SEGNPR	INT11
+#define SIG_STACKV	INT12
+#define SIG_GENPFV	INT13
+#define SIG_PAGEFV	INT14
+#define SIG_SEGFLT	INT15
+#define SIG_MATHFV	INT16
+
+#define PIT		IRQ0
+#define KEYBOARD	IRQ1
+#define CASCADE		IRQ2
+#define COM2		IRQ3
+#define	COM1		IRQ4
+#define LPT2		IRQ5
+#define FLOPPY		IRQ6
+#define LPT1		IRQ7
+#define CMOS		IRQ8
+#define FREE1		IRQ9
+#define FREE2		IRQ10
+#define FREE3		IRQ11
+#define PS2_MOUSE	IRQ12
+#define FPU		IRQ13
+#define PATA_IDE	IRQ14
+#define SATA_IDE	IRQ15
 
 struct registers
 {
@@ -41,7 +60,6 @@ struct registers
 };
 
 typedef void (*isr_t)(struct registers *);
-
 void register_interrupt_handler(uint8_t n, isr_t handler);
 
-#endif
+#endif /* _ISR_H_ */
