3b486e42c4d50687ad094cb438b2be7397bfc5d3
salmabad <srodr063@fiu.edu>
2013-01-27 12:26:17 -0500

    changing DPRINTK to printk

--- kernel/isr.c
+++ kernel/isr.c
@@ -3,13 +3,13 @@
 #include <kernel/types.h>
 #include <kernel/printk.h>
 
-isr_t interrupt_handlers[256];
+isr_t interrupt_handlers[INTS];
 
 void isr_handler(struct registers regs)
 {
 	isr_t handler;
 
-	printk("recieved interrupt: %d\n", regs.int_no);
+	DPRINTK("recieved interrupt: %d\n", regs.int_no);
 
 	send_eoi_master();
 
@@ -24,13 +24,9 @@ void irq_handler(struct registers regs)
 {
 	isr_t handler;
 
-	// Send an EOI signal to PICs
-
-	// If this interrupt involved the slave:
 	if (regs.int_no >= 40)
 		send_eoi_slave();
 
-	// Send EOI signal to master, regardless.
 	send_eoi_master();
 
 	if (interrupt_handlers[regs.int_no])
