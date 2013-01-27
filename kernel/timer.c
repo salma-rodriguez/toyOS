35bd7bd3dff6b324ace12d1f996f973a63c27c75
salmabad <srodr063@fiu.edu>
2013-01-27 12:25:05 -0500

    changing DPRINTK to printk

--- kernel/timer.c
+++ kernel/timer.c
@@ -4,11 +4,9 @@
 #include <kernel/timer.h>
 #include <kernel/printk.h>
 
-#define CLOCK IRQ0
-
 uint32_t tick;
 
-void timer_callback(struct registers regs)
+void handle_pit_irq(struct registers *regs)
 {
 	tick++;
 	DPRINTK("Tick: %d\n", tick);
@@ -19,10 +17,9 @@ void init_timer(uint32_t frequency)
 	uint8_t l, h;
 	uint32_t divisor;
 
-	DPRINTK("timer...\t");
+	printk("timer...\t\t");
 
 	tick = 0;
-	register_interrupt_handler(CLOCK, (isr_t)timer_callback);
 
 	divisor = 1193180 / frequency;
 	outportb(0x43, 0x36);
@@ -33,5 +30,5 @@ void init_timer(uint32_t frequency)
 	outportb(0x40, l);
 	outportb(0x40, h);
 
-	DPRINTK("done!\n");
+	printk("done!\n");
 }
