affa06d8c56f19b119593d9cf11a5995aa46699f
salmabad <srodr063@fiu.edu>
2013-01-27 12:32:59 -0500

    adding interrupt definitions

---
+++ include/asm/int_defs.h
@@ -0,0 +1,41 @@
+#ifndef _INT_DEFS_H_
+#define _INT_DEFS_H_
+
+#define INT0		0x00
+#define INT1		0x01
+#define INT2		0x02
+#define INT3		0x03
+#define	INT4		0X04
+#define INT5		0x05
+#define INT6		0x06
+#define INT7		0X07
+#define	INT8		0x08
+#define INT9		0x09
+#define INT10		0x0A
+#define INT11		0x0B
+#define INT12		0x0C
+#define INT13		0x0D
+#define INT14		0x0E
+#define INT15		0x0F
+#define INT16		0X10
+
+#define IRQ0		0x20
+#define IRQ1		0x21
+#define IRQ2		0x22
+#define IRQ3		0x23
+#define IRQ4		0x24
+#define IRQ5		0x25
+#define IRQ6		0x26
+#define IRQ7		0x27
+#define IRQ8		0x28
+#define IRQ9		0x29
+#define IRQ10		0x2A
+#define IRQ11		0x2B
+#define IRQ12		0x2C
+#define IRQ13		0x2D
+#define IRQ14		0x2E
+#define IRQ15		0x2F
+
+#define INTS		256
+
+#endif /* _INT_DEFS_H_ */
