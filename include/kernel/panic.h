ca7890c6ffcfd98e1961417d3e99c6635e1cfea8
salmabad <srodr063@fiu.edu>
2013-01-27 10:33:52 -0500

    moving PANIC definition to another line

--- include/kernel/panic.h
+++ include/kernel/panic.h
@@ -1,8 +1,8 @@
 #ifndef _PANIC_H_
 #define _PANIC_H_
 
-#define PANIC(fmt, ...) panic(fmt, ## __VA_ARGS__);
-
 void panic(const char *fmt, ...);
+
+#define PANIC(fmt, ...) panic(fmt, ## __VA_ARGS__);
 
 #endif /* _PANIC_H_ */
