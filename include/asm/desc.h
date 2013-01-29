92849c933603fdf1f134c9f8f64130e9a55e1ea6
salmabad <srodr063@fiu.edu>
2013-01-28 22:02:33 -0500

    renaming stuff

--- include/asm/desc.h
+++ include/asm/desc.h
@@ -4,27 +4,23 @@
 #include <asm/segment.h>
 #include <asm/desc_defs.h>
 
-#define GDT_ENTRIES 5
-#define IDT_ENTRIES 256
-
 // This structure contains the value of one GDT entry.
 // We use the attribute 'pacdked' to tell GCC not to change
 // any of the aligment in the structure
 
-void init_descriptor_tables();
 
 #define SET_GDT_BASE(base) 					\
 	.base_l = base & 0xFFFF, 				\
 	.base_m = (base >> 0x10) & 0xFF, 			\
 	.base_h = (base >> 0x18) & 0xFF
 
-#define SET_GDT_LOW(lim) .lim_lo = lim & 0xFFFF
+#define SET_GDT_LIMIT(lim) .lim_lo = lim & 0xFFFF
 
-#define SET_GDT_DPL(seg, dpl) \
-	.privge = seg? GDT_DATA_PL(dpl): GDT_CODE_PL(dpl)
+#define SET_GDT_ACCESS(seg, dpl) \
+	.access = seg? GDT_DATA_PL(dpl): GDT_CODE_PL(dpl)
 
-#define SET_GDT_GRAN(lim, len) \
-	.granty = ((lim & 0xFFFF) | ((GRANULARITY(len)) & 0xF0))
+#define SET_GDT_GRANULARITY(lim, len) \
+	.granty = (((lim >> 0x10) & 0xFFFF) | ((GRANULARITY(len)) & 0xF0))
 
 #define SET_IDT_BASE(base)					\
 	.base_l = base & 0xFFFF,				\
@@ -39,10 +35,10 @@ void init_descriptor_tables();
 #define GDT_SET_GATE(dpl, seg, base, lim) ({			\
 	gdt_entry_t val;					\
 	val = (gdt_entry_t) { 					\
-		SET_GDT_LOW(lim),				\
 		SET_GDT_BASE(base),				\
-		SET_GDT_DPL(seg, dpl),				\
-		SET_GDT_GRAN(lim, 0xF)				\
+		SET_GDT_LIMIT(lim),				\
+		SET_GDT_ACCESS(seg, dpl),			\
+		SET_GDT_GRANULARITY(lim, 0xF)			\
 	}; val;							\
 })
 
