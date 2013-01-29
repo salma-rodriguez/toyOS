f6b20802ef270dd44f68cb9d1f8c0dc487588e15
salmabad <srodr063@fiu.edu>
2013-01-28 22:04:55 -0500

    moving stuff around

--- include/kernel/tables.h
+++ include/kernel/tables.h
@@ -1,9 +1,12 @@
 #ifndef _GDT_H_
 #define _GDT_H_
 
-#define PAGE_SIZE 1 << 12
-
 #include <asm/desc.h>
+
+#define PAGE_SIZE 	(1 << 12)
+
+#define GDT_ENTRIES 	5
+#define IDT_ENTRIES 	256
 
 struct gdt_page
 {
@@ -16,6 +19,8 @@ struct idt_page
 	idt_ptr_t idt_ptr;
 	idt_entry_t idt[IDT_ENTRIES];
 } __attribute__ ((aligned(PAGE_SIZE)));
+
+void init_descriptor_tables();
 
 typedef struct gdt_page gdt_page_t;
 typedef struct idt_page idt_page_t;
