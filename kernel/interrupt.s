1fb2c71dfeb10d0103ad56ca29cd7ae7e2271208
salmabad <srodr063@fiu.edu>
2013-01-30 19:31:22 -0500

    moving gdt and idt flush to another file

--- kernel/interrupt.s
+++ kernel/interrupt.s
@@ -1,26 +1,3 @@
-global idt_flush
-
-idt_flush:
-	mov eax, [esp+4]
-	lidt [eax]
-	ret
-
-global gdt_flush
-
-gdt_flush:
-	mov eax, [esp+4]
-	lgdt [eax]
-
-	mov ax, 0x10
-	mov ds, ax
-	mov es, ax
-	mov fs, ax
-	mov gs, ax
-	mov ss, ax
-	jmp 0x08:.flush
-.flush:
-	ret
-
 %macro ISR_NOERRCODE 1
 	global isr%1
 	isr%1:
