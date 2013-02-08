916994f75fb8bcbbe486209d33d488bd94c0a419
salmabad <srodr063@fiu.edu>
2013-02-08 14:06:31 -0500

    working on memory allocation

--- include/kernel/heap.h
+++ include/kernel/heap.h
@@ -1,9 +1,42 @@
 #ifndef _KHEAP_H_
 #define _KHEAP_H_
 
+#define KHEAP_START		0xC0000000
+#define KHEAP_INITIAL_SIZE	0x100000
+#define HEAP_INDEX_SIZE		0x20000
+#define HEAP_MAGIC		0x123890AB
+#define HEAP_MIN_SIZE		0x70000
+
+struct header
+{
+	uint32_t magic;
+	uint8_t is_hole;
+	uint32_t size;
+}
+
+struct footer
+{
+	uint32_t magic;
+	struct header *header;
+}
+
+struct heap
+{
+	struct ordered_array *index;
+	uint32_t start_address;
+	uint32_t end_address;
+	uint32_t max_address;
+	uint8_t supervisor;
+	uint8_t readonly;
+}
+
 uint32_t kmalloc(size_t siz);
 uint32_t kmalloc_a(size_t siz);
 uint32_t kmalloc_p(size_t siz, uint32_t *phys);
 uint32_t kmalloc_ap(size_t siz, uint32_t *phys);
+
+struct heap *create_heap(uint32_t, uint32_t, uint32_t, uint8_t, uint8_t);
+void *alloc(uint32_t size, uint8_t page_align, struct heap *heap);
+void free(void *p, struct heap *heap);
 
 #endif /* _KHEAP_H_ */
--- mm/heap.c
+++ mm/heap.c
@@ -1,8 +1,5 @@
 #include <kernel/types.h>
 
-/** right now: just a very simple implementation
- * slab allocators will be implemented later **/
-
 extern uint32_t end;
 uint32_t placement_addr = (uint32_t)&end;
 
@@ -48,4 +45,35 @@ static uint32_t kmalloc_int(size_t siz, int align, uint32_t *phys)
 	if (phys) *phys = placement_addr;
 	temp = PLACE(siz);
 	return temp;
+}
+
+static int32_t find_smallest_hole(size_t size, uint8_t page_align, struct heap *heap)
+{
+	int i;
+	off_t offset;
+	ssize_t hole_size;
+	uint32_t location;
+	struct header *header
+
+	i=0;
+	while (i < heap->index->size) {
+		header = (struct header *)lookup_ordered_array(i, heap->index);
+		if (page_align > 0) {
+			location = (uint32_t)header;
+			offset = 0;
+			if ((location+sizeof(struct header)) & 0xFFFFF000 != 0)
+				offset = PAGE_SIZE - (location+sizeof(struct header))%PAGE_SIZE;
+			hole_size = (ssize_t)header->size - offset;
+			if (hole_size >= (ssize_t)size)
+				break;
+		} else if (header->size >= size)
+			break;
+		i++;
+	}
+
+	if (i == heap->index->size)
+		return -1;
+
+	return i;
+
 }
