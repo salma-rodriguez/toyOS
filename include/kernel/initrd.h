c47aa277d02df0d017140f957a45f711d3663d86
salma-rodriguez <srodr063@fiu.edu>
2013-03-09 00:02:54 -0500

    fixing bugs

--- include/kernel/initrd.h
+++ include/kernel/initrd.h
@@ -2,20 +2,19 @@
 #define _INITRD_H_
 
 #include <kernel/fs.h>
-#include <kernel/common.h>
 
-struct initrd
+struct initrd_header
 {
         uint32_t nfiles;
-}
+};
 
 struct initrd_file_header
 {
+        off_t offset;
         uint8_t magic;
         int8_t name[64];
-        unsigned off_t offset;
         uint32_t length;
-}
+};
 
 struct inode *initialize_initrd(uint32_t location);
 
--- init/initrd.c
+++ init/initrd.c
@@ -1,3 +1,5 @@
+#include <string.h>
+#include <kernel/heap.h>
 #include <kernel/initrd.h>
 
 struct initrd_header *initrd_header;
@@ -11,9 +13,10 @@ int nroot_nodes;
 
 struct dirent dirent;
 
-static uint32_t initrd_read(struct inode *node, unsigned off_t offset, uint32_t size, uint8_t *buffer)
+static uint32_t initrd_read(struct inode *node, off_t offset, size_t size, uint8_t *buffer)
 {
-        struct initrd_file_header = file_headers[node->inode];
+        struct initrd_file_header header;
+        header = file_headers[node->inode];
         if (offset > header.length)
                 return 0;
         if (offset+size > header.length)
@@ -36,7 +39,7 @@ static struct dirent *initrd_readdir(struct inode *inode, uint32_t index)
                 return 0;
         strcpy(dirent.name, root_nodes[index-1].name);
         dirent.name[strlen(root_nodes[index-1].name)] = 0;
-        dirent.ino = root-nodes[index-1].inode;
+        dirent.ino = root_nodes[index-1].inode;
         return &dirent;
 }
 
@@ -55,35 +58,35 @@ struct inode *initialize_initrd(uint32_t location)
 {
         int i;
         // root directory
-        initrd_header = (struct initrd_header *)locatioln;
-        file_headers = (initrd_file_header *)(location+sizeof(struct initrd_header));
+        initrd_header = (struct initrd_header *)location;
+        file_headers = (struct initrd_file_header *)(location+sizeof(struct initrd_header));
         initrd_root = (struct inode *)kmalloc(sizeof(struct inode));
-        initrd_root->mask = initrd_root->uid = NULL;
-        initrd_root->gid = NULL;
-        initrd_root->inode = NULL;
-        initrd_root->length = NULL;
+        initrd_root->mask = initrd_root->uid = 0;
+        initrd_root->gid = 0;
+        initrd_root->inode = 0;
+        initrd_root->length = 0;
         initrd_root->flags = FS_DIRECTORY;
-        initrd_root->read = NULL;
-        initrd_root->write = NULL;
-        initrd_root->open = NULL;
-        initrd_root->close = NULL;
+        initrd_root->read = 0;
+        initrd_root->write = 0;
+        initrd_root->open = 0;
+        initrd_root->close = 0;
         initrd_root->readdir =  &initrd_readdir;
         initrd_root->finddir = &initrd_finddir;
-        initrd_root->ptr = NULL;
-        initrd_root->impl = NULL;
+        initrd_root->ptr = 0;
+        initrd_root->impl = 0;
         // /dev directory
         initrd_dev = (struct inode *)kmalloc(sizeof(struct inode));
         strcpy(initrd_dev->name, "dev");
-        initrd_dev->mask = NULL;
-        initrd_dev->uid = NULL;
-        initrd_dev->gid = NULL;
-        initrd_dev->inode = NULL;
-        initrd_dev->length = NULL;
+        initrd_dev->mask = 0;
+        initrd_dev->uid = 0;
+        initrd_dev->gid = 0;
+        initrd_dev->inode = 0;
+        initrd_dev->length = 0;
         initrd_dev->flags = FS_DIRECTORY;
-        initrd_dev->read = NULL;
-        initrd_dev->write = NULL;
-        initrd_dev->open = NULL;
-        initrd_dev->close = NULL;
+        initrd_dev->read = 0;
+        initrd_dev->write = 0;
+        initrd_dev->open = 0;
+        initrd_dev->close = 0;
         initrd_dev->readdir = &initrd_readdir;
         initrd_dev->finddir = &initrd_finddir;
         initrd_dev->ptr = 0;
@@ -95,19 +98,19 @@ struct inode *initialize_initrd(uint32_t location)
         for (i = 0; i < initrd_header->nfiles; i++)
         {
                 file_headers[i].offset += location;
-                strcpy(root_nodes[i].name, &file_headers[i].name);
-                root_nodes[i].mask = root_nodes[i].uid = NULL;
-                root_nodes[i].gid = NULL;
+                strcpy(root_nodes[i].name, (char *)&file_headers[i].name);
+                root_nodes[i].mask = root_nodes[i].uid = 0;
+                root_nodes[i].gid = 0;
                 root_nodes[i].length = file_headers[i].length;
                 root_nodes[i].inode = i;
                 root_nodes[i].flags = FS_FILE;
                 root_nodes[i].read = &initrd_read;
-                root_nodes[i].write = NULL;
-                root_nodes[i].readdir = NULL;
-                root_nodes[i].finddir = NULL;
-                root_nodes[i].open = NULL;
-                root_nodes[i].close = NULL;
-                root_nodes[i].impl = NULL;
+                root_nodes[i].write = 0;
+                root_nodes[i].readdir = 0;
+                root_nodes[i].finddir = 0;
+                root_nodes[i].open = 0;
+                root_nodes[i].close = 0;
+                root_nodes[i].impl = 0;
         }
 
         return initrd_root;
