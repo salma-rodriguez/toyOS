4263578b55ec23e0bded9a65bf267b6f353b2093
salma-rodriguez <srodr063@fiu.edu>
2013-03-09 00:02:36 -0500

    fixing some bugs

--- fs/fs.c
+++ fs/fs.c
@@ -1,54 +1,46 @@
 #include <kernel/fs.h>
+#include <kernel/types.h>
 
-
-struct inode *fs_root = 0;
+struct inode *fs_root = NULL;
 
 uint32_t read_fs(struct inode *node, off_t offset, size_t size, uint8_t *buffer)
 {
         if (node->read != 0)
                 return node->read(node, offset, size, buffer);
-        else
-                return 0;
+        return 0;
 }
 
-uint32_t open(struct inode *node, off_t offset, size_t size, uint8_t *buffer)
+void open_fs(struct inode *node)
 {
         if (node->read != 0)
-                return node->read(node, offset, size, buffer);
-        else
-                return 0;
+                node->open(node);
 }
 
-uint32_t close(struct inode *node, off_t offset, size_t size, uint8_t *buffer)
+void close_fs(struct inode *node)
 {
         if (node->read != 0)
-                return node->read(node, offset, size, buffer);
-        else
-                return 0;
+                node->close(node);
 }
 
-uint32_t write(struct inode *node, off_t offset, size_t size, uint8_t *buffer)
+uint32_t write_fs(struct inode *node, off_t offset, size_t size, uint8_t *buffer)
 {
         if (node->read != 0)
-                return node->read(node, offset, size, buffer);
-        else
-                return 0;
+                return node->write(node, offset, size, buffer);
+        return 0;
 }
 
-uint32_t readdir(struct inode *node, off_t offset, size_t size, uint8_t *buffer)
+struct dirent *readdir_fs(struct inode *node, uint32_t index)
 {
         if ((node->flags&0x7) == FS_DIRECTORY && node->readdir != 0)
                 if (node->read != 0)
-                        return node->read(node, offset, size, buffer);
-                else
-                        return 0;
+                        return node->readdir(node, index);
+        return NULL;
 }
 
-uint32_t finddir(struct inode *node, off_t offset, size_t size, uint8_t *buffer)
+struct inode *finddir_fs(struct inode *node, char *name)
 {        
         if ((node->flags&0x7) == FS_DIRECTORY && node->readdir != 0)
                 if (node->read != 0)
-                        return node->read(node, offset, size, buffer);
-                else
-                        return 0;
+                        return node->finddir(node, name);
+        return NULL;
 }
--- include/kernel/fs.h
+++ include/kernel/fs.h
@@ -1,5 +1,4 @@
-#include <types.h>
-#include <kernel/fs.h>
+#include <kernel/types.h>
 
 #define FS_FILE		0x01
 #define FS_DIRECTORY	0x02
@@ -10,30 +9,30 @@
 #define FS_MOUNTPOINT	0x08
 
 struct inode;
-extern inode *fs_root;
+extern struct inode *fs_root;
 
 typedef uint32_t (*read_t)
-(struct inode *, uint32_t, uint32_t, uint8_t *);
+(struct inode *, off_t, size_t, uint8_t *);
 
 typedef uint32_t (*write_t)
-(struct inode *, uint32_t, uint32_t, uint8_t *);
+(struct inode *, off_t, size_t, uint8_t *);
 
 typedef void (*open_t)(struct inode *);
 
 typedef void (*close_t)(struct inode *);
 
-typedef struct dirent * (*readdir_t);
+typedef struct dirent * (*readdir_t)
 (struct inode *, uint32_t);
 
 typedef struct inode * (*finddir_t)
 (struct inode *, char *name); 
 
-uint32_t read_fs(struct inode *, unsigned off_t, size_t, uint8_t);
-uint32_t write_fs(struct inode *, unsigned off_t, size_t, uint8_t);
-void open_fs(struct inode *, uint8_t read, uint8_t write);
+uint32_t read_fs(struct inode *, off_t, size_t, uint8_t *);
+uint32_t write_fs(struct inode *, off_t, size_t, uint8_t *);
+void open_fs(struct inode *);
 void close_fs(struct inode *);
 struct dirent *readdir_fs(struct inode *, uint32_t);
-struct inode *finddir_fs(struct inode *, char *name);
+struct inode *finddir_fs(struct inode *, char *);
 
 struct inode
 {
@@ -61,4 +60,4 @@ struct dirent
 {
 	char name[128];
 	uint32_t ino;
-}
+};
