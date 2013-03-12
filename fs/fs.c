6158167aacd5341ac67e5548f18e82114151b881
salma-rodriguez <srodr063@fiu.edu>
2013-03-11 21:49:33 -0400

    moving stuff around

--- fs/fs.c
+++ fs/fs.c
@@ -3,44 +3,44 @@
 
 struct fs_node *fs_root = NULL;
 
-uint32_t read_fs(struct fs_node *node, off_t offset, size_t size, uint8_t *buffer)
-{
-        if (node->read != 0)
-                return node->read(node, offset, size, buffer);
-        return 0;
-}
-
 void open_fs(struct fs_node *node)
 {
-        if (node->read != 0)
+        if (node->open != NULL)
                 node->open(node);
 }
 
 void close_fs(struct fs_node *node)
 {
-        if (node->read != 0)
+        if (node->close != NULL)
                 node->close(node);
+}
+
+uint32_t read_fs(struct fs_node *node, off_t offset, size_t size, uint8_t *buffer)
+{
+        if (node->read != NULL)
+                return node->read(node, offset, size, buffer);
+        return 0;
 }
 
 uint32_t write_fs(struct fs_node *node, off_t offset, size_t size, uint8_t *buffer)
 {
-        if (node->read != 0)
+        if (node->write != NULL)
                 return node->write(node, offset, size, buffer);
         return 0;
 }
 
 struct dirent *readdir_fs(struct fs_node *node, uint32_t index)
 {
-        if ((node->flags&0x7) == FS_DIRECTORY && node->readdir != 0)
-                if (node->read != 0)
+        if ((node->flags&0x7) == FS_DIRECTORY && node->readdir != NULL)
+                if (node->readdir != 0)
                         return node->readdir(node, index);
         return NULL;
 }
 
 struct fs_node *finddir_fs(struct fs_node *node, char *name)
 {        
-        if ((node->flags&0x7) == FS_DIRECTORY && node->readdir != 0)
-                if (node->read != 0)
+        if ((node->flags&0x7) == FS_DIRECTORY && node->readdir != NULL)
+                if (node->finddir != 0)
                         return node->finddir(node, name);
         return NULL;
 }
--- include/kernel/fs.h
+++ include/kernel/fs.h
@@ -1,3 +1,6 @@
+#ifndef _FS_H_
+#define _FS_H_
+
 #include <kernel/types.h>
 
 #define FS_FILE		0x01
@@ -8,24 +11,42 @@
 #define FS_SYMLINK	0x06
 #define FS_MOUNTPOINT	0x08
 
+struct dirent;
 struct fs_node;
-extern struct fs_node *fs_root;
 
-typedef uint32_t (*read_t)
-(struct fs_node *, off_t, size_t, uint8_t *);
+typedef void (*oc_t) (struct fs_node *);
+typedef struct fs_node *(*finddir_t) (struct fs_node *, char *);
+typedef struct dirent *(*readdir_t) (struct fs_node *, uint32_t);
+typedef uint32_t (*rw_t) (struct fs_node *, off_t, size_t, uint8_t *);
 
-typedef uint32_t (*write_t)
-(struct fs_node *, off_t, size_t, uint8_t *);
+struct fs_node
+{
+	uint32_t uid;
+	uint32_t gid;
+	uint32_t impl;
+	uint32_t mask;
+	uint32_t flags;
+	uint32_t inode;
+	uint32_t length;
+	char name[128];
 
-typedef void (*open_t)(struct fs_node *);
+	oc_t open;
+	oc_t close;
+	rw_t read;
+	rw_t write;
+	readdir_t readdir;
+	finddir_t finddir;
 
-typedef void (*close_t)(struct fs_node *);
+	struct fs_node *ptr;
+};
 
-typedef struct dirent * (*readdir_t)
-(struct fs_node *, uint32_t);
+struct dirent
+{
+	char name[128];
+	uint32_t ino;
+};
 
-typedef struct fs_node * (*finddir_t)
-(struct fs_node *, char *name); 
+extern struct fs_node *fs_root;  
 
 uint32_t read_fs(struct fs_node *, off_t, size_t, uint8_t *);
 uint32_t write_fs(struct fs_node *, off_t, size_t, uint8_t *);
@@ -34,30 +55,4 @@ void close_fs(struct fs_node *);
 struct dirent *readdir_fs(struct fs_node *, uint32_t);
 struct fs_node *finddir_fs(struct fs_node *, char *);
 
-struct fs_node
-{
-	char name[128];
-	uint32_t mask;
-	uint32_t uid;
-	uint32_t gid;
-	uint32_t flags;
-	uint32_t inode;
-	uint32_t length;
-	uint32_t impl;
-
-	read_t read;
-	write_t write;
-	open_t open;
-	close_t close;
-	readdir_t readdir;
-	finddir_t finddir;
-	struct fs_node *ptr;
-};
-
-
-
-struct dirent
-{
-	char name[128];
-	uint32_t ino;
-};
+#endif /* _FS_H_ */
