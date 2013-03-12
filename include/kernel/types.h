201a5e8f60bba549dce422823e1b05247864f870
salma-rodriguez <srodr063@fiu.edu>
2013-03-11 21:51:01 -0400

    adding signed offset type

--- include/kernel/types.h
+++ include/kernel/types.h
@@ -26,7 +26,8 @@ typedef 	__u16 	uint16_t;
 typedef 	__u32 	uint32_t;
 typedef		__u64	uint64_t;
 
-typedef int off_t;
+typedef int soff_t;
+typedef unsigned int off_t;
 typedef int ssize_t;
 typedef unsigned int size_t;
 
