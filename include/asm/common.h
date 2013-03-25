0e6461502828bc29a14d638c4fea78bcae3ea9c9
salma-rodriguez <srodr063@fiu.edu>
2013-03-25 13:28:23 -0400

    adding code to fork, do context switch, etc.

--- include/asm/common.h
+++ include/asm/common.h
@@ -30,6 +30,16 @@ static inline void enable_interrupts()
 static inline void disable_interrupts()
 {
 	__asm__ __volatile__ ("cli");
-}
+}                                                        
+
+#ifndef offsetof
+#define offsetof(TYPE, MEMBER) ((size_t) & ((TYPE *)0)->MEMBER)
+#endif
+
+#ifndef container_of
+#define container_of(ptr, type, member) ({                      \
+        const typeof(((type *)0)->member) * __mptr = (ptr);     \
+        (type *)((char *)__mptr - offsetof(type, member)); })
+#endif
 
 #endif /* _COMMON_H_*/
