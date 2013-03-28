17965efae1c0e66badc9b3e4134f727934029c48
salma-rodriguez <srodr063@fiu.edu>
2013-03-27 21:57:17 -0400

    no changes, really...

--- sched/task.c
+++ sched/task.c
@@ -28,31 +28,32 @@ void move_stack(void *new_stack_start, uint32_t size)
                 i -= 0x1000)
         {
                 alloc_frame(get_page(i, 1, current_directory), 0, 1);
-                flush_tlb();
+        }
 
-                asm volatile("mov %%esp, %0" : "=r" (old_stack_ptr));
-                asm volatile("mov %%ebp, %0" : "=r" (old_base_ptr));
+        flush_tlb();
 
-                offset = (uint32_t)new_stack_start - initial_esp;
-                new_stack_ptr = old_stack_ptr + offset;
-                new_base_ptr = old_base_ptr + offset;
-                
-                memcpy((void *)new_stack_ptr, (void *)old_stack_ptr, initial_esp - old_stack_ptr);
+        asm volatile("mov %%esp, %0" : "=r" (old_stack_ptr));
+        asm volatile("mov %%ebp, %0" : "=r" (old_base_ptr));
 
-                for (i = (uint32_t)new_stack_start; i > (uint32_t)new_stack_start-size; i -= 4)
+        offset = (uint32_t)new_stack_start - initial_esp;
+        new_stack_ptr = old_stack_ptr + offset;
+        new_base_ptr = old_base_ptr + offset;
+
+        memcpy((void *)new_stack_ptr, (void *)old_stack_ptr, initial_esp - old_stack_ptr);
+
+        for (i = (uint32_t)new_stack_start; i > (uint32_t)new_stack_start-size; i -= 4)
+        {
+                uint32_t tmp, *tmp2;
+
+                tmp = *(uint32_t *)i;
+                if ((old_stack_ptr < tmp) && (tmp < initial_esp))
                 {
-                        uint32_t tmp, *tmp2;
+                        tmp = tmp + offset;
+                        tmp2 = (uint32_t *)i;
+                        *tmp2 = tmp;
 
-                        tmp = *(uint32_t *)i;
-                        if ((old_stack_ptr < tmp) && (tmp < initial_esp))
-                        {
-                                tmp = tmp + offset;
-                                tmp2 = (uint32_t *)i;
-                                *tmp2 = tmp;
-
-                                asm volatile("movl %0, %%esp" :: "r" (new_stack_ptr));
-                                asm volatile("movl %0, %%ebp" :: "r" (new_base_ptr));
-                        }
+                        asm volatile("movl %0, %%esp" :: "r" (new_stack_ptr));
+                        asm volatile("movl %0, %%ebp" :: "r" (new_base_ptr));
                 }
         }
 }
